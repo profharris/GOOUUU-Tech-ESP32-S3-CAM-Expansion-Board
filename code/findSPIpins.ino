874c915e30f05bc99b2288e29a407c9f250b2cdc
profharris <profharris@users.noreply.github.com>
2025-05-20 00:11:33 -0500

    Update findSPIpins.ino

--- code/findSPIpins.ino
+++ code/findSPIpins.ino
@@ -1,1 +1,24 @@
 
+// findSPIpins.ino
+
+// Find the default (Hardware) SPI pins for your board.
+// Make sure you have the right board selected in:&nbsp; `Tools —» Boards`...
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);         // Initialize Serial communication
+  while(!Serial);               // Wait for the Serial port to open
+  
+  Serial.print(" MOSI: ");
+  Serial.println(MOSI);
+  Serial.print(" MISO: ");
+  Serial.println(MISO);
+  Serial.print("  SCK: ");
+  Serial.println(SCK);
+  Serial.print("SS/CS: ");
+  Serial.println(SS);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
